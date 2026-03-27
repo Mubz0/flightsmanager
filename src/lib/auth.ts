6665e3ffafd09f6abc540ec2317d142fd8008982
Mubarak.D <mubarak.dirie@googlemail.com>
2026-03-27 10:18:46 +0700

    feat: add firebase auth helper functions

---
+++ src/lib/auth.ts
@@ -0,0 +1,29 @@
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut as firebaseSignOut,
+} from "firebase/auth";
+import { firebaseApp } from "./firebase";
+
+export const auth = getAuth(firebaseApp);
+
+const googleProvider = new GoogleAuthProvider();
+
+export async function signInWithGoogle() {
+  return signInWithPopup(auth, googleProvider);
+}
+
+export async function signInWithEmail(email: string, password: string) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export async function signUpWithEmail(email: string, password: string) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export async function signOut() {
+  return firebaseSignOut(auth);
+}
